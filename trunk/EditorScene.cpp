da073632dbc4d23a7082673ddb91eaa47890f4fd
mnbayazit <mnbayazit@de4b8b76-9504-11de-8746-af773c80d763>
2009-08-30 02:29:47 +0000

    created stubs for mouse events

---
+++ trunk/EditorScene.cpp
@@ -0,0 +1,16 @@
+/* 
+ * File:   EditorScene.cpp
+ * Author: mark
+ * 
+ * Created on August 29, 2009, 7:23 PM
+ */
+
+#include "EditorScene.h"
+
+EditorScene::mousePressEvent(QGraphicsSceneMouseEvent* mouseEvent) {
+
+}
+
+EditorScene::mouseMoveEvent(QGraphicsSceneMouseEvent* mouseEvent) {
+    
+}
