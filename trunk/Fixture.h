0fb5e4e6edcd81cb3d057772f2796a9a2cda15e1
mnbayazit <mnbayazit@de4b8b76-9504-11de-8746-af773c80d763>
2009-12-17 01:48:10 +0000

    and this

---
+++ trunk/Fixture.h
@@ -0,0 +1,71 @@
+/* 
+ * File:   Fixture.h
+ * Author: mark
+ *
+ * Created on September 21, 2009, 12:42 PM
+ */
+
+#ifndef _FIXTURE_H
+#define	_FIXTURE_H
+
+#include "Object.h"
+#include "Polygon.h"
+
+struct FixtureDef : public b2FixtureDef {
+};
+
+class Fixture : public Object {
+    friend class Body;
+    Q_OBJECT
+public:
+private:
+    Fixture(b2Fixture *fix, const FixtureDef& def, QObject* parent = NULL);
+    b2Fixture *b2fixture();
+    b2Fixture *m_b2fixture;
+};
+
+
+/*
+class Fixture : public Object {
+    Q_OBJECT
+    Q_PROPERTY(Polygon polygon READ polygon WRITE setPolygon)
+    Q_PROPERTY(qreal friction READ friction WRITE setFriction)
+    Q_PROPERTY(qreal restitution READ restitution WRITE setRestitution)
+    Q_PROPERTY(qreal density READ density WRITE setDensity)
+    Q_PROPERTY(bool isSensor READ isSensor WRITE setSensor)
+
+public:
+    Fixture(QObject *parent = NULL);
+    Fixture(const Fixture&);
+    Fixture& operator=(const Fixture&);
+    operator b2FixtureDef() const;
+
+    // READ
+    Polygon polygon() const;
+    qreal friction() const;
+    qreal restitution() const;
+    qreal density() const;
+    bool isSensor() const;
+
+    // WRITE
+    void setPolygon(const Polygon&);
+    void setFriction(qreal);
+    void setRestitution(qreal);
+    void setDensity(qreal);
+    void setSensor(bool);
+
+private:
+    Polygon m_polygon;
+    qreal m_friction;
+    qreal m_restitution;
+    qreal m_density;
+    bool m_isSensor;
+};
+
+Q_DECLARE_METATYPE(Fixture)
+*/
+
+
+
+#endif	/* _FIXTURE_H */
+
